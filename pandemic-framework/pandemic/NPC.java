59791744c495bdaa422376dffbc1a6a7db5c0440
NickBlakW <71640764+NickBlakW@users.noreply.github.com>
2020-10-27 12:40:58 +0100

    Update NPC.java

--- pandemic-framework/pandemic/NPC.java
+++ pandemic-framework/pandemic/NPC.java
@@ -4,5 +4,26 @@ public class NPC {
 
     private String name;
 
+    private String quest;
 
+    public NPC(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void NPCTalk(String quest)
+    {
+        System.out.println("I need " + quest);
+        this.quest = quest;
+    }
+    
+    public String getQuest()
+    {
+        return quest;
+    }
 }
