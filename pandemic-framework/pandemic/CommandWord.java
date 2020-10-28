e8af52fa39b8d6a851f60dc4d1fa6d0ffd53473e
NickBlakW <71640764+NickBlakW@users.noreply.github.com>
2020-10-28 11:24:36 +0100

    Update CommandWord.java

--- pandemic-framework/pandemic/CommandWord.java
+++ pandemic-framework/pandemic/CommandWord.java
@@ -2,7 +2,7 @@ package pandemic;
 
 public enum CommandWord
 {
-    GO("go"), QUIT("quit"), HELP("help"), PICKUP("pickup"), USE("use"), UNKNOWN("?");
+    GO("go"), QUIT("quit"), HELP("help"), PICKUP("pickup"), USE("use"), GIVE("give"), UNKNOWN("?");
     
     private String commandString;
     
