76b2c082364128bb0247a4f6226c550501ecfbd0
Daniel Bahrami <71696167+danielbahrami@users.noreply.github.com>
2020-11-01 14:25:58 +0100

    Room.java

--- pandemic-framework/pandemic/Room.java
+++ pandemic-framework/pandemic/Room.java
@@ -35,7 +35,7 @@ public class Room
 
     public String getLongDescription()
     {
-        return "You are in" + description + ".\n" + getExitString();
+        return "You are in " + description + ".\n" + getExitString();
     }
 
     private String getExitString()
