db19279719f706c08143d9e372d8acd8eea977a7
NickBlakW <NickBlakW@users.noreply.github.com>
2020-11-24 10:57:23 +0100

    Commit

--- pandemic-framework/pandemic/NPC.java
+++ pandemic-framework/pandemic/NPC.java
@@ -8,18 +8,24 @@ public class NPC {
     private Item questItem;
     private String quest;
     private HashMap<String, NPC> NPCList;
+    private Sprite sprite;
+    private String spriteName;
 
-    public NPC(String name, String quest, Item questItem)
+    public NPC(String name, String quest, Item questItem, String spriteName)
     {
         this.name = name;
         this.quest = quest;
         this.questItem = questItem;
         this.NPCList = new HashMap<String, NPC>();
+        this.spriteName = spriteName;
+
+        this.sprite = new Sprite();
+        sprite.setImage(spriteName);
     }
 
     public void addNPCs()
     {
-        this.NPCList.put(this.name, new NPC(this.name, this.quest, getQuestItem()));
+        this.NPCList.put(this.name, new NPC(this.name, this.quest, getQuestItem(), this.spriteName));
     }
 
     public String getQuest()
