ec58631dceb64b4e7eb61dec1828a6df5cfada00
NickBlakW <NickBlakW@users.noreply.github.com>
2020-11-28 18:16:25 +0100

    Commit

--- pandemic-framework/GUI/PandemicController.java
+++ pandemic-framework/GUI/PandemicController.java
@@ -1,13 +1,18 @@
 package GUI;
 
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import pandemic.Player;
 import pandemic.Sprite;
 
 public class PandemicController {
 
     @FXML
-    public Player player;
+    private Player player;
+    private Sprite walls;
+    private Sprite item;
 
     @FXML
     public void createPlayer(Player player)
@@ -19,10 +24,37 @@ public class PandemicController {
 
     // Create sprite. spriteName, positionX and positionY
     @FXML
-    public void createSprite(String imageName, double x, double y)
+    public void createItem(String itemImage, double x, double y)
     {
         Sprite sprite = new Sprite();
-        sprite.setImage(imageName);
+        sprite.setImage(itemImage);
         sprite.setPosition(x, y);
+        sprite.setVelocity(0,0);
+    }
+
+    @FXML
+    public void createBackground(String backGroundImage, Sprite walls)
+    {
+        Image image = new Image(backGroundImage);
+        Sprite wall = new Sprite();
+        this.walls = walls;
+        wall.setImage(walls.toString());
+        wall.setPosition(0,0);
+        wall.setVelocity(0,0);
+    }
+
+
+
+    @FXML
+    public void move()
+    {
+        new AnimationTimer()
+        {
+            @Override
+            public void handle(long l)
+            {
+
+            }
+        }.start();
     }
 }
