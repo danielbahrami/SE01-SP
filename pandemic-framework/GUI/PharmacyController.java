1c0fa11ec0b1cba91a39fdabe26183e7f7319264
Nicolai Walther <niwal19@student.sdu.dk>
2020-12-08 12:19:05 +0100

    Commit

--- pandemic-framework/GUI/PharmacyController.java
+++ pandemic-framework/GUI/PharmacyController.java
@@ -5,12 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import org.w3c.dom.css.ElementCSSInlineStyle;
 import pandemic.Item;
 
 public class PharmacyController {
@@ -70,8 +68,10 @@ public class PharmacyController {
             Item item = this.item;
             ic.addToInventory(item);
             System.out.println(ic.getInventory());
+            itemButton.setDisable(true);
+
             if (ic.getInventory().contains(item)) {
-                Pharmacy.fxml.getChildren().remove(Pills.png);
+
             }
         }
     }
